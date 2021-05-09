a5bbfdcb5444c995517d0076e8900736dc03a5df
dbwc <d.benseman@wc.school.nz>
2021-05-10 09:28:09 +1200

    Class Lists now done

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ def create_connection(db_file):
     return None
 
 
+def get_classes():
+    con = create_connection(DB_NAME)
+    query = "SELECT id, class_name" \
+            " FROM tutor_classes ORDER BY class_name ASC"
+    cur = con.cursor()  # You need this line next
+    cur.execute(query)  # this line actually executes the query
+    class_list = cur.fetchall()  # puts the results into a list usable in python
+    con.close()
+    return class_list
+
+
 @app.route('/')
 def render_homepage():
     # connect to the database
@@ -29,7 +40,23 @@ def render_homepage():
     student_list = cur.fetchall()  # puts the results into a list usable in python
     con.close()
 
-    return render_template('home.html', students=student_list)
+    class_list = get_classes()
+    return render_template('home.html', students=student_list, classes=class_list)
+
+
+@app.route('/class/<tutor_id>')
+def render_class(tutor_id):
+    # connect to the database
+    con = create_connection(DB_NAME)
+    query = "SELECT id, year_level, schl_id, surname, first_name, tutor_id, email" \
+            " FROM students WHERE tutor_id=? ORDER BY surname, first_name ASC"
+    cur = con.cursor()  # You need this line next
+    cur.execute(query, (tutor_id, ))  # this line actually executes the query
+    student_list = cur.fetchall()  # puts the results into a list usable in python
+    con.close()
+
+    class_list = get_classes()
+    return render_template('class.html', students=student_list, classes=class_list)
 
 
 if __name__ == '__main__':
